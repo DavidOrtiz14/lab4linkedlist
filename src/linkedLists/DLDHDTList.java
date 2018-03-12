866e65dd22af159616ebbffb056fed3c2f1344fa
ICOM STUDENTS <david.ortiz14@upr.edu>
2018-03-12 13:17:38 -0400

    Implements

---
+++ src/linkedLists/DLDHDTList.java
@@ -0,0 +1,145 @@
+package linkedLists;
+
+import java.util.NoSuchElementException;
+
+import linkedLists.AbstractSLList.SNode;
+
+public class DLDHDTList<E> extends AbstractDLList<E> {
+	private DNode<E> header, trailer; 
+	private int length; 
+	
+	public DLDHDTList() { 
+		// ADD CODE HERE to generate empty linked list of this type 
+	}
+	
+	public void addFirstNode(Node<E> nuevo) {
+		addNodeAfter(header, nuevo); 
+	}
+	
+	public void addLastNode(Node<E> nuevo) { 
+		DNode<E> dnuevo = (DNode<E>) nuevo; 
+		DNode<E> nBefore = trailer.getPrev();  
+		nBefore.setNext(dnuevo); 
+		trailer.setPrev(dnuevo); 
+		dnuevo.setPrev(nBefore); 
+		dnuevo.setNext(trailer); 
+		length++; 
+	}
+
+	public void addNodeAfter(Node<E> target, Node<E> nuevo) {
+		DNode<E> dnuevo = (DNode<E>) nuevo; 
+		DNode<E> nBefore = (DNode<E>) target; 
+		DNode<E> nAfter = nBefore.getNext(); 
+		nBefore.setNext(dnuevo); 
+		nAfter.setPrev(dnuevo); 
+		dnuevo.setPrev(nBefore); 
+		dnuevo.setNext(nAfter); 
+		length++; 
+	}
+
+	public void addNodeBefore(Node<E> target, Node<E> nuevo) {
+		if(((DNode <E>)target).getPrev()==header){
+			((DNode <E>)nuevo).setNext(((DNode <E>)target));
+		}
+		else{
+			((DNode <E>)nuevo).setPrev(((DNode <E>)target).getPrev());
+			((DNode <E>)nuevo).setNext(((DNode <E>)target));
+		
+		}
+	}
+
+	public Node<E> createNewNode() {
+		return new DNode<E>();
+	}
+
+	public Node<E> getFirstNode() throws NoSuchElementException {
+		if (length == 0) 
+			throw new NoSuchElementException("List is empty."); 
+		return header.getNext();
+	}
+
+	public Node<E> getLastNode() throws NoSuchElementException {
+		if (length == 0) 
+			throw new NoSuchElementException("List is empty."); 
+		return trailer.getPrev();
+	}
+
+	public Node<E> getNodeAfter(Node<E> target)
+			throws NoSuchElementException {
+		if(((DNode <E>)target).getNext()==trailer){
+			throw new NoSuchElementException("No such element.");
+		}else
+		{
+		return ((DNode <E>)target).getNext(); 
+		}
+	}
+
+	public Node<E> getNodeBefore(Node<E> target)
+			throws NoSuchElementException {
+		if(((DNode <E>)target).getPrev()==header){
+			throw new NoSuchElementException("No such element.");
+		}else{
+			return ((DNode <E>)target).getPrev(); 
+		}
+		
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public void removeNode(Node<E> target) {
+		if(((DNode <E>)target).getPrev()==header){
+			header = ((DNode <E>)target).getNext(); 
+		}
+		else if(((DNode <E>)target).getNext()==trailer){
+			trailer =((DNode <E>)target).getPrev();
+		}
+		else { 
+			SNode<E> prevNode = (SNode<E>) this.getNodeBefore(target); 
+			prevNode.setNext(((SNode<E>) target).getNext()); 
+		}
+		((SNode<E>) target).clean();   // clear all references from target
+		length--; 
+	}
+	
+	/**
+	 * Prepares every node so that the garbage collector can free 
+	 * its memory space, at least from the point of view of the
+	 * list. This method is supposed to be used whenever the 
+	 * list object is not going to be used anymore. Removes all
+	 * physical nodes (data nodes and control nodes, if any)
+	 * from the linked list
+	 */
+	private void destroy() {
+		while (header != null) { 
+			DNode<E> nnode = header.getNext(); 
+			header.clean(); 
+			header = nnode; 
+		}
+	}
+	
+	/**
+	 * The execution of this method removes all the data nodes from
+	 * the current instance of the list, leaving it as a valid empty
+	 * doubly linked list with dummy header and dummy trailer nodes. 
+	 */
+	public void makeEmpty() { 
+		while (header.getNext() != null) { 
+			DNode<E> nnode = header.getNext(); 
+			header.setElement(null); 
+			header.setNext(null); 
+			header = nnode; 
+		}
+		length = 0; 
+	}
+		
+	protected void finalize() throws Throwable {
+	    try {
+			this.destroy(); 
+	    } finally {
+	        super.finalize();
+	    }
+	}
+
+}
